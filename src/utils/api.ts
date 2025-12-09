d357c508e983cf0933fe4810139bf2c6d6eca33a
woongminKi <dndals1991@gmail.com>
2025-12-10 00:23:00 +0900

    chore : 백엔드 api 환경변수 적용

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:4000/api';
+const API_BASE = process.env.REACT_APP_API_URL || 'http://localhost:4000';
+const API_BASE_URL = `${API_BASE}/api`;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
