74022e05af8badc93520c3e1cf40100757b81948
woongminKi <dndals1991@gmail.com>
2026-01-02 17:48:56 +0900

    fix : 메인 홈 이미지 부분 로직 수정

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -242,7 +242,7 @@ export const adminContentAPI = {
     title?: string;
     buttonText?: string;
     buttonLink?: string;
-  }) => api.patch('/admin/content/hero', data),
+  }) => api.put('/admin/content/hero', data),
 
   updateSchoolPassers: (schoolPassers: Array<{
     thumbnailUrl: string;
